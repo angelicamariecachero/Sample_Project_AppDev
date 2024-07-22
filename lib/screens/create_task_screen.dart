68a0d994f64f8b1b49e0f18a0eb4414b8bdc5b46
Angelica Marie Cachero <angelicamariecachero@gmail.com>
2024-07-22 15:40:17 +0800

    Update and rename statistics_screen.dart to create_task_screen.dart

--- lib/screens/create_task_screen.dart
+++ lib/screens/create_task_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class StatisticsScreen extends StatelessWidget {
+class CreateTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        'Statistics',
+        'CreateTask',
         style: TextStyle(fontSize: 24),
       ),
     );
